188c7cc8780f617d4f9eb457de57adb720e99fdd
onecloudacc <58681401+onecloudacc@users.noreply.github.com>
2020-01-18 20:58:14 +0530

    Add files via upload

---
+++ eurekaClient2Verb/src/main/java/com/learn/springCloud/controller/VerbController.java
@@ -0,0 +1,38 @@
+package com.learn.springCloud.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author rajes
+ * date    Apr 7, 20171:27:33 AM
+ *
+ */
+@RestController
+public class VerbController {
+	private static final Logger log = LoggerFactory.getLogger(VerbController.class);
+	
+	@Value("${words}")
+	private String words;
+
+	 @RequestMapping("/")
+	  public @ResponseBody String getWord() {
+		
+	    String[] wordArray = words.split(",");
+	    int i = (int)Math.round(Math.random() * (wordArray.length - 1));
+	    log.info(String.valueOf(i));
+	    log.info(wordArray[i]);
+	    return wordArray[i];
+	  }
+	 
+	 @RequestMapping(value="/testFeign")
+	 public @ResponseBody String getCustomWord() {
+		 System.out.println("called by feign client");
+		 return "Successs";
+				
+	 }
+}
